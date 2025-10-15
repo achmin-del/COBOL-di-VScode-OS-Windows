3923aed82b83b327155587d8d28de1f85136bb16
Achmad Amin <achmadamin010@gmail.com>
2025-10-15 21:23:59 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scrolling untuk table of contents
+document.querySelectorAll(".toc a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  });
+});
